a70588708d975d3f29bc4e2623d2d05876f53f45
mohdragon010 <mohammed.ayman152433@gmail.com>
2024-11-29 09:46:45 -0800

    Update main.js

--- main page/main.js
+++ main page/main.js
@@ -3,5 +3,5 @@ console.log("Page Loaded. GPS placeholder is ready for future content.");
 // Check if the user is logged in
 if (localStorage.getItem('isLoggedIn') !== 'true') {
     // Redirect to the login page if not logged in
-    window.location.href = '../login.html';
+    window.location.href = '../index.html';
 }
